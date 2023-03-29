e522ef92ed5f23ef2adfaaacecc408a891d374f2
Gerard McCann <gerard.mccann@kadence.co>
2023-03-29 21:09:03 +0100

    Try try again…

--- doStuff.groovy
+++ doStuff.groovy
@@ -1,19 +1,28 @@
 pipeline {
-  agent {
-    docker {
-      label 'amd'
-      image 'cypress/base:latest'
+    agent {
+        docker {
+            label 'amd'
+            // image 'cypress/base:latest'
+            // image 'cypress/included:cypress-12.8.1-node-18.14.1-chrome-111.0.5563.110-1-ff-111.0-edge-111.0.1661.51-1'
+            image 'ubuntu-2004:202010-01'
+        }
     }
-  }
+    
+    stages {
+        stage('build and test') {
+            steps {
+                sh 'ls'
+                // sh 'docker run -it -v $PWD:/e2e -w /e2e cypress/base:latest'
+                // sh 'npm ci'
+                // sh "npx cypress run"
+                sh './doStuff.sh'
+            }
+        }
+    }
 
-  stages {
-    stage('build and test') {
-      steps {
-        sh 'ls'
-        // sh 'docker run -it -v $PWD:/e2e -w /e2e cypress/base:latest'
-        sh 'npm ci'
-        sh "npx cypress run"
-      }
+    post {
+        always {
+            echo ****************in post**************
+        }
     }
-  }
 }
